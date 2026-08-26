0846b287b58420441f6803648b2f8895fd8d0031
Noyal Ashwin J <noyalashwin0704@gmail.com>
2026-08-26 22:33:50 +0530

    Time: 5 ms (83.12%), Space: 77.4 MB (49.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,21 +3,21 @@ class Solution {
         int low = 0;
         int high = height.length - 1;
         int maxarea = 0;
-
+        
         while (low < high) {
             int width = high - low;
             int length = Math.min(height[low], height[high]);
             int area = width * length;
-
+            
             maxarea = Math.max(maxarea, area);
-
+            
             if (height[low] < height[high]) {
                 low++;
             } else {
                 high--;
             }
         }
-
+        
         return maxarea;
     }
 }
