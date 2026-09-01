f54eabc157ccf98a7cc8be0ef256a2de2fe53e8e
Noyal Ashwin J <noyalashwin0704@gmail.com>
2026-09-01 10:09:06 +0530

    Time: 2 ms (95.5%), Space: 45.7 MB (68.08%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,38 +1,22 @@
-import java.util.*;
-
 class Solution {
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
         List<List<Integer>> answer = new ArrayList<>();
-
         find(candidates, target, 0, new ArrayList<>(), answer);
-
         return answer;
     }
-
     static void find(int[] arr, int target, int index,
                      List<Integer> list,
                      List<List<Integer>> answer) {
-
-
         if (target == 0) {
             answer.add(new ArrayList<>(list));
             return;
         }
-
-
         if (target < 0 || index == arr.length) {
             return;
         }
-
-
         list.add(arr[index]);
-
         find(arr, target - arr[index], index, list, answer);
-
         list.remove(list.size() - 1);
-
         find(arr, target, index + 1, list, answer);
     }
 }
