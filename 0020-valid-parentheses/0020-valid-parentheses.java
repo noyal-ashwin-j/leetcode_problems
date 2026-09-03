6643ea944998b1968ca51802d83da2dfad1e9ecb
Noyal Ashwin J <noyalashwin0704@gmail.com>
2026-09-03 09:14:15 +0530

    Time: 3 ms (86.18%), Space: 43.4 MB (40.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,41 +2,30 @@ import java.util.*;
 
 class Solution {
     public boolean isValid(String s) {
-
         Stack<Character> stack = new Stack<>();
-
         for (int i = 0; i < s.length(); i++) {
-
             char ch = s.charAt(i);
-
             // Opening brackets
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             }
-
             // Closing brackets
             else {
-
                 if (stack.isEmpty()) {
                     return false;
                 }
-
                 char top = stack.pop();
-
                 if (ch == ')' && top != '(') {
                     return false;
                 }
-
                 if (ch == '}' && top != '{') {
                     return false;
                 }
-
                 if (ch == ']' && top != '[') {
                     return false;
                 }
             }
         }
-
         return stack.isEmpty();
     }
 }
